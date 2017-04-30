b99829bab3751ee43f6ad9602cf2ef6528af0002
Miguel Flores Ruiz de Eguino <miguel.frde@gmail.com>
2017-04-30 15:17:47 -0700

    Add batch normalization to CNN

--- assignment2/cs231n/classifiers/cnn.py
+++ assignment2/cs231n/classifiers/cnn.py
@@ -19,7 +19,7 @@ class ThreeLayerConvNet(object):
 
     def __init__(self, input_dim=(3, 32, 32), num_filters=32, filter_size=7,
                  hidden_dim=100, num_classes=10, weight_scale=1e-3, reg=0.0,
-                 dtype=np.float32):
+                 dtype=np.float32, use_batchnorm=False):
         """
         Initialize a new network.
 
@@ -50,10 +50,14 @@ class ThreeLayerConvNet(object):
         ############################################################################
         self.pool_param = {'pool_height': 2, 'pool_width': 2, 'stride': 2}
         self.conv_param = {'stride': 1, 'pad': (filter_size - 1) // 2}
+        self.use_batchnorm = use_batchnorm
 
         C, H, W = input_dim
         self.params['W1'] = np.random.normal(scale=weight_scale, size=(num_filters, C, filter_size, filter_size))
         self.params['b1'] = np.zeros(num_filters)
+        if self.use_batchnorm:
+            self.params['gamma1'] = np.ones(num_filters)
+            self.params['beta1'] = np.zeros(num_filters)
 
         H_prime = 1 + (H + 2*self.conv_param['pad'] - filter_size) // self.conv_param['stride']
         W_prime = 1 + (W + 2*self.conv_param['pad'] - filter_size) // self.conv_param['stride']
@@ -63,6 +67,9 @@ class ThreeLayerConvNet(object):
 
         self.params['W2'] = np.random.normal(scale=weight_scale, size=(conv_out_dim, hidden_dim))
         self.params['b2'] = np.zeros(hidden_dim)
+        if self.use_batchnorm:
+            self.params['gamma2'] = np.ones(hidden_dim)
+            self.params['beta2'] = np.zeros(hidden_dim)
 
         self.params['W3'] = np.random.normal(scale=weight_scale, size=(hidden_dim, num_classes))
         self.params['b3'] = np.zeros(num_classes)
@@ -89,9 +96,23 @@ class ThreeLayerConvNet(object):
         # computing the class scores for X and storing them in the scores          #
         # variable.                                                                #
         ############################################################################
-        out_conv, cache_conv = conv_relu_pool_forward(X, W1, b1, self.conv_param, self.pool_param)
-        out_fc1, cache_fc1 = affine_relu_forward(out_conv, W2, b2)
-        scores, scores_cache = affine_forward(out_fc1, W3, b3)
+        bn_param = [{'mode': 'test' if y is None else 'train'} for i in range(2)]
+        cache = {}
+
+        out_conv, cache['conv'] = conv_forward_fast(X, W1, b1, self.conv_param)
+        out_conv, cache['conv_relu'] = relu_forward(out_conv)
+        if self.use_batchnorm:
+            out_conv, cache['bn_conv'] = spatial_batchnorm_forward(
+                    out_conv, self.params['gamma1'], self.params['beta1'], bn_param[0])
+        out_conv, cache['conv_pool'] = max_pool_forward_fast(out_conv, self.pool_param)
+
+        out_fc, cache['fc'] = affine_forward(out_conv, W2, b2)
+        if self.use_batchnorm:
+            out_fc, cache['bn_fc'] = batchnorm_forward(
+                    out_fc, self.params['gamma2'], self.params['beta2'], bn_param[1])
+        out_fc, cache['fc_relu'] = relu_forward(out_fc)
+
+        scores, cache['scores'] = affine_forward(out_fc, W3, b3)
         ############################################################################
         #                             END OF YOUR CODE                             #
         ############################################################################
@@ -107,9 +128,21 @@ class ThreeLayerConvNet(object):
         # for self.params[k]. Don't forget to add L2 regularization!               #
         ############################################################################
         loss, dscores = softmax_loss(scores, y)
-        dfc1, grads['W3'], grads['b3'] = affine_backward(dscores, scores_cache)
-        dconv, grads['W2'], grads['b2'] = affine_relu_backward(dfc1, cache_fc1)
-        dX, grads['W1'], grads['b1'] = conv_relu_pool_backward(dconv, cache_conv)
+
+        dfc, grads['W3'], grads['b3'] = affine_backward(dscores, cache['scores'])
+
+        dconv = relu_backward(dfc, cache['fc_relu'])
+        if self.use_batchnorm:
+            dconv, grads['gamma2'], grads['beta2'] = batchnorm_backward(
+                    dconv, cache['bn_fc'])
+        dconv, grads['W2'], grads['b2'] = affine_backward(dconv, cache['fc'])
+
+        dX = max_pool_backward_fast(dconv, cache['conv_pool'])
+        if self.use_batchnorm:
+            dX, grads['gamma1'], grads['beta1'] = spatial_batchnorm_backward(
+                    dX, cache['bn_conv'])
+        dX = relu_backward(dX, cache['conv_relu'])
+        dX, grads['W1'], grads['b1'] = conv_backward_fast(dX, cache['conv'])
 
         loss += 0.5 * self.reg * (np.sum(W1*W1) + np.sum(W2*W2) + np.sum(W3*W3))
         for i in (1, 2, 3):
