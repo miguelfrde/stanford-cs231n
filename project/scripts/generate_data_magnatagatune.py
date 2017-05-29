9a2801295354a8f35d641448f3e6206a2a77861d
Miguel Flores Ruiz de Eguino <miguel.frde@gmail.com>
2017-05-29 13:10:54 -0700

    Update preprocessing for magnatagatune

--- project/scripts/generate_data_magnatagatune.py
+++ project/scripts/generate_data_magnatagatune.py
@@ -1,11 +1,12 @@
 import csv
+import multiprocessing
 import os
 import _pickle as pickle
 
 import click
 import librosa
 import numpy as np
-import tensorflow as tf
+from sklearn.model_selection import train_test_split
 
 
 CLASSES = [
@@ -20,7 +21,7 @@ CLASSES = [
 ]
 CLASSES_DICT = {c: i for i, c in enumerate(CLASSES)}
 DEFAULT_SHAPE = (128, 628)
-
+pool = None  # Global pool intialized in __main__
 
 
 def get_class_vector(genres):
@@ -40,18 +41,16 @@ def fix_shape(spectogram, default_shape=DEFAULT_SHAPE):
     return spectogram
 
 
-def process_song(song_path, genres, overwrite=False):
+def process_song(song_path, index, destination_dir, overwrite=False):
     basename, extension = os.path.splitext(song_path)
     if extension not in ('.mp3', '.au'):
-        return None, None
+        return
     if not os.path.isfile(song_path):
         print('NOT FOUND: ', song_path)
-        return None, None
-    picklefile = basename + '.pickle'
+        return
+    picklefile = str(index) + '.pickle'
     if not overwrite and os.path.isfile(picklefile):
-        with open(picklefile, 'rb') as f:
-            spectogram, class_vector = pickle.load(f)
-            return fix_shape(spectogram), class_vector
+        return
     try:
         y, sr = librosa.load(song_path, mono=True)
     except:
@@ -59,54 +58,95 @@ def process_song(song_path, genres, overwrite=False):
         return None, None
     spectogram = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=128, n_fft=2048, hop_length=1024)
     spectogram = librosa.power_to_db(spectogram, ref=np.max)
-    class_vector = get_class_vector(genres)
-    with open(basename + '.pickle', 'wb') as f:
-        f.write(pickle.dumps((spectogram, class_vector)))
-        f.close()
-    return fix_shape(spectogram).T, class_vector
+    write_pickle(os.path.join(destination_dir, picklefile), fix_shape(spectogram).T)
 
 
-def process_songs(dirname, overwrite=False):
+def get_annotations_dict(dirname):
     annotations_csv = os.path.join(dirname, 'annotations_final.csv')
-    genre_vecs = []
-    spectograms = []
+    d = {}
     with open(annotations_csv, 'r') as tsvfile:
         reader = csv.DictReader(tsvfile, dialect='excel-tab')
         for i, row in enumerate(reader):
+            full_path_song = os.path.join(dirname, row['mp3_path'])
+            if not os.path.isfile(full_path_song):
+                print('NOT FOUND: ', row['mp3_path'])
             genres = [g for g in CLASSES if row[g] == '1']
             if not genres:
                 continue
-            full_path_song = os.path.join(dirname, row['mp3_path'])
-            spectogram, genre_vec = process_song(full_path_song, genres, overwrite=overwrite)
-            if spectogram is None:
-                continue
-            spectograms.append(spectogram)
-            genre_vecs.append(genre_vec)
-    return spectograms, genre_vecs
+            d[row['mp3_path']] = get_class_vector(genres)
+    return d
+
+
+def split_annotations(annotations_dict, percent_test=0.2, percent_val=0.2, percent_dev=0.2):
+    X_filenames = list(annotations_dict.keys())
+    y = [annotations_dict[x] for x in X_filenames]
+    X_train, X_test, y_train, y_test = train_test_split(X_filenames, y, test_size=percent_test)
+    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=percent_val)
+    _, X_dev, _, y_dev = train_test_split(X_filenames, y, test_size=percent_dev)
+    return X_train, y_train, X_test, y_test, X_val, y_val, X_dev, y_dev
+
+
+def create_dir(path, dirname):
+    new_dir = os.path.join(path, dirname)
+    if not os.path.exists(new_dir):
+        os.makedirs(new_dir)
+
+
+def write_pickle(filename, data):
+    with open(filename, 'wb') as f:
+        f.write(pickle.dumps(data))
+        f.close()
+
+
+def save_partial_dataset(index, dirname, X, full_path, overwrite):
+    n = len(X)
+    W = multiprocessing.cpu_count()
+    lo = int(index*(n / W))
+    hi = int((index + 1)*(n / W))
+    print('  Processing %d to %d out of %d' % (lo, hi - 1, n))
+    for i, x in enumerate(X[lo:hi]):
+        process_song(os.path.join(dirname, x), i + lo, full_path, overwrite=overwrite)
+    return True
+
+
+def save_dataset(dirname, dataset_type, X, y, overwrite=False):
+    global pool
+    full_path = os.path.join(dirname, dataset_type)
+    labels_file = os.path.join(dirname, dataset_type, 'labels.pickle')
+    filenames_file = os.path.join(dirname, dataset_type, 'filenames.pickle')
+
+    if not os.path.isfile(labels_file) or overwrite:
+        write_pickle(labels_file, y)
+    if not os.path.isfile(filenames_file) or overwrite:
+        write_pickle(filenames_file, X)
+
+    results = []
+    print('Processing %s dataset' % (dataset_type,))
+    for i in range(multiprocessing.cpu_count()):
+        results.append(pool.apply_async(save_partial_dataset, args=(i, dirname, X, full_path, overwrite)))
+    assert all([r.get() for r in results])
 
 
 @click.command()
 @click.argument('dataset_dir', nargs=1)
 @click.option('--overwrite', default=False, is_flag=True)
-def main(dataset_dir, overwrite):
-    spectograms, classes = process_songs(dataset_dir, overwrite=overwrite)
-    print(len(spectograms), spectograms[0].shape)
-    print(len(classes), classes[0].shape)
-    X = np.vstack(spectograms).reshape((len(spectograms),) + DEFAULT_SHAPE)
-    y = np.vstack(classes).reshape((len(spectograms), len(CLASSES)))
-    print(X.shape)
-    print(y.shape)
-    assert np.array_equal(X[0], spectograms[0])
-    X = X.transpose(0, 2, 1)
-    with tf.Graph().as_default():
-        X_init = tf.placeholder(tf.float32, shape=X.shape)
-        y_init = tf.placeholder(tf.float32, shape=y.shape)
-        X_data = tf.Variable(X_init, name='X_data')
-        y_data = tf.Variable(y_init, name='y_data')
-        saver = tf.train.Saver()
-        with tf.Session() as sess:
-            sess.run(tf.global_variables_initializer(), feed_dict={X_init: X, y_init: y})
-            saver.save(sess, os.path.join(dataset_dir, 'data.ckpt'))
+@click.option('--percent-val', default=0.2, type=float)
+@click.option('--percent-test', default=0.2, type=float)
+@click.option('--percent-dev', default=0.2, type=float)
+def main(dataset_dir, overwrite, percent_val, percent_test, percent_dev):
+    annotations_dict = get_annotations_dict(dataset_dir)
+    create_dir(dataset_dir, 'train')
+    create_dir(dataset_dir, 'test')
+    create_dir(dataset_dir, 'val')
+    create_dir(dataset_dir, 'dev')
+    X_train, y_train, X_test, y_test, X_val, y_val, X_dev, y_dev = split_annotations(
+        annotations_dict, percent_test=percent_test, percent_val=percent_val, percent_dev=percent_dev)
+    save_dataset(dataset_dir, 'train', X_train, y_train, overwrite=overwrite)
+    save_dataset(dataset_dir, 'test', X_test, y_test, overwrite=overwrite)
+    save_dataset(dataset_dir, 'val', X_val, y_val, overwrite=overwrite)
+    save_dataset(dataset_dir, 'dev', X_dev, y_dev, overwrite=overwrite)
+
 
 if __name__ == '__main__':
+    pool = multiprocessing.Pool(multiprocessing.cpu_count())
     main()
